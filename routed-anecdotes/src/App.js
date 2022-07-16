04df6f8db4e6efb8d22fdd78ae8bc3cb45f50bca
akutuh <akutuh@uef.fi>
2022-07-16 23:20:59 +0300

    routed anecdotes 7.1

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
 
 const Menu = () => {
   const padding = {
@@ -6,9 +12,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -122,14 +128,22 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  //<AnecdoteList anecdotes={anecdotes} />
+  //<About />
+  //<CreateNew addNew={addNew} />
+
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      <Router>
+        <h1>Software anecdotes</h1>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/create" element={<CreateNew addNew={addNew} />}/>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   )
 }
