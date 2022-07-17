a0fa7ebaa74e6f729104284a2a161a35fabb846f
akutuh <akutuh@uef.fi>
2022-07-17 21:27:53 +0300

    ultimate hooks 7.8

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -18,14 +18,22 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll =  () => {
+    axios
+      .get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
-    create
+    create,
+    getAll
   }
 
   return [
@@ -41,6 +49,11 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+  }, [])
+
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
@@ -49,6 +62,7 @@ const App = () => {
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
+    
   }
 
   return (
